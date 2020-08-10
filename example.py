e27e93c15df8f4a380fa812592e8e0bd31c9ba1b
Eduard Christian Dumitrescu <eduard.c.dumitrescu@gmail.com>
2020-08-10 19:18:12 -0400

    blacken example.py

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
-
 import ctypes
 from ctypes import c_byte, c_int, Structure, POINTER
+
 
 class StokenBruteForceAssist(Structure):
     _fields_ = [
@@ -15,26 +15,26 @@ class StokenBruteForceAssist(Structure):
     @property
     def code_out_str(self):
         s = bytes(self.code_out)
-        return s[:s.find(0)].decode("ascii")
+        return s[: s.find(0)].decode("ascii")
+
 
 bfasst = ctypes.cdll.LoadLibrary("./libstoken_bfasst.so")
-bfasst.stoken_bfasst_generate_passcode.argtypes = [
-    POINTER(StokenBruteForceAssist)
-]
+bfasst.stoken_bfasst_generate_passcode.argtypes = [POINTER(StokenBruteForceAssist)]
+
 
 def main():
     ass = StokenBruteForceAssist()
     ass.pin[:5] = b"1234\0"
     ass.digits = 8
     ass.key_time_offset = 0
-    ass.seed[:16] = b"x"*16
-    ass.time_blocks[:16*5] = b"y"*(16*5)
+    ass.seed[:16] = b"x" * 16
+    ass.time_blocks[: 16 * 5] = b"y" * (16 * 5)
     ret = bfasst.stoken_bfasst_generate_passcode(ass)
     if ret != 0:
         raise ValueError("bad return code {}".format(ret))
     if ass.code_out_str != "94968463":
         raise ValueError("bad output code")
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
